b9840e9b1ea9ca96d907b746c9a93e8a5c4e152d
JEAN.HORACIO <JEAN.HORACIO@WKSBCK05.intranet.local>
2019-05-22 16:27:02 -0300

    3 Commit

--- processa.php
+++ processa.php
@@ -2,11 +2,11 @@
 
 include_once ("conexao.php");
 
-$nome  = filter_input(INPUT_POST, 'nomesoftware',FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_POST, 'email',FILTER_SANITIZE_EMAIL);
+$nome  = filter_input(INPUT_POST, 'ncelular',FILTER_SANITIZE_STRING);
+$linha = filter_input(INPUT_POST, 'linha',FILTER_SANITIZE_EMAIL);
 
-//echo "Nome: $nome <br>";
-//echo "Email: $email <br>";
+echo "ncelular: $nome  <br>";
+echo "linha:    $linha <br>";
 
-$result  = "INSERT INTO LENTI (usuario,email)VALUES ('$nome','$email')";
-$result_usu  = mysqli_query($conn,$result);
+//$result  = "INSERT INTO user (usuario,senha)VALUES ('$nome','$linha')";
+//$result_usu  = mysqli_query($sql,$result);
